a58b185cb1bffbfeab40e0139d12c2b9d01e3166
ZarcxCliff <113826447+ZarcxCliff@users.noreply.github.com>
2023-03-13 19:14:22 +0100

    Update SpeakHello.js

--- module4-solution/easier/SpeakHello.js
+++ module4-solution/easier/SpeakHello.js
@@ -23,7 +23,7 @@
   var helloSpeaker = {};
   var speakWord = "Hello ";
    helloSpeaker.speak = function (name) {
-    console.log(speakWord + " " + helloSpeaker.name);
+    console.log(speakWord + " " + name);
   }
   window.helloSpeaker = helloSpeaker;
 })(window);
