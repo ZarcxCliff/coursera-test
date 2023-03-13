4a3010439c4761e7ec69231b85b8f99640a7e467
ZarcxCliff <113826447+ZarcxCliff@users.noreply.github.com>
2023-03-13 19:13:46 +0100

    Update SpeakGoodBye.js

--- module4-solution/easier/SpeakGoodBye.js
+++ module4-solution/easier/SpeakGoodBye.js
@@ -24,7 +24,7 @@
   var byeSpeaker = {};
   var speakWord = "Good Bye ";
   byeSpeaker.speak = function (name) {
-    console.log(speakWord + " " + byeSpeaker.name);
+    console.log(speakWord + " " + name);
   }
   window.byeSpeaker = byeSpeaker;
 })(window);
